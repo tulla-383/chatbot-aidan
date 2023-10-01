fc5cb587144cb51a9ecaba0fdb70c6074a2f1432
tulla-383 <145882693+tulla-383@users.noreply.github.com>
2023-10-01 01:52:38 -0400

    Update aidan.js

--- aidan.js
+++ aidan.js
@@ -35,40 +35,39 @@ function poserQuestion(question) {
     // et poser la question suivante en fonction de sa réponse.
 
     // Exemple : poser la question suivante
-if (
-  reponseUtilisateur === "restaurant" ||
-  reponseUtilisateur === "bistrot" ||
-  reponseUtilisateur === "brasserie" ||
-  reponseUtilisateur === "auberge" ||
-  reponseUtilisateur === "guinguette" ||
-  reponseUtilisateur === "créperie" ||
-  reponseUtilisateur === "bouchon" ||
-  reponseUtilisateur === "taverne" ||
-  reponseUtilisateur === "grill" ||
-  reponseUtilisateur === "cantine" ||
-  reponseUtilisateur === "boui-boui" ||
-  reponseUtilisateur === "snack" ||
-  reponseUtilisateur === "rôtisserie" ||
-  reponseUtilisateur === "café" ||
-  reponseUtilisateur === "trattoria" ||
-  reponseUtilisateur === "pizzeria" ||
-  reponseUtilisateur === "tiki bar" ||
-  reponseUtilisateur === "resto" ||
-  reponseUtilisateur === "cuisine locale" ||
-  reponseUtilisateur === "bungalow gourmand" ||
-  reponseUtilisateur === "échoppe" ||
-  reponseUtilisateur === "barbecue" ||
-  reponseUtilisateur === "cuisine de rue" ||
-  reponseUtilisateur === "traiteur" ||
-  reponseUtilisateur === "épicurienne" ||
-  reponseUtilisateur === "taque" ||
-  reponseUtilisateur === "table d'hôte" ||
-  reponseUtilisateur === "lolo" ||
-  reponseUtilisateur === "paillote" ||
-  reponseUtilisateur === "food truck"
-) {
-  poserQuestion("Quel est le nom de votre établissement ?");
-}
+    if (
+      reponseUtilisateur === "restaurant" ||
+      reponseUtilisateur === "bistrot" ||
+      reponseUtilisateur === "brasserie" ||
+      reponseUtilisateur === "auberge" ||
+      reponseUtilisateur === "guinguette" ||
+      reponseUtilisateur === "créperie" ||
+      reponseUtilisateur === "bouchon" ||
+      reponseUtilisateur === "taverne" ||
+      reponseUtilisateur === "grill" ||
+      reponseUtilisateur === "cantine" ||
+      reponseUtilisateur === "boui-boui" ||
+      reponseUtilisateur === "snack" ||
+      reponseUtilisateur === "rôtisserie" ||
+      reponseUtilisateur === "café" ||
+      reponseUtilisateur === "trattoria" ||
+      reponseUtilisateur === "pizzeria" ||
+      reponseUtilisateur === "tiki bar" ||
+      reponseUtilisateur === "resto" ||
+      reponseUtilisateur === "cuisine locale" ||
+      reponseUtilisateur === "bungalow gourmand" ||
+      reponseUtilisateur === "échoppe" ||
+      reponseUtilisateur === "barbecue" ||
+      reponseUtilisateur === "cuisine de rue" ||
+      reponseUtilisateur === "traiteur" ||
+      reponseUtilisateur === "épicurienne" ||
+      reponseUtilisateur === "taque" ||
+      reponseUtilisateur === "table d'hôte" ||
+      reponseUtilisateur === "lolo" ||
+      reponseUtilisateur === "paillote" ||
+      reponseUtilisateur === "food truck"
+    ) {
+      poserQuestion("Quel est le nom de votre établissement ?");
     } else if (reponseUtilisateur === "airbnb") {
       // Poser des questions spécifiques aux propriétaires Airbnb...
     } else if (reponseUtilisateur === "location de voitures") {
