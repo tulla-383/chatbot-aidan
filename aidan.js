77785f0994a6240f9a8d6a7f67913652213dfa91
tulla-383 <145882693+tulla-383@users.noreply.github.com>
2023-10-01 10:49:02 -0400

    Update aidan.js

--- aidan.js
+++ aidan.js
@@ -3,6 +3,7 @@ const chatbot = document.getElementById('chatbot');
 const conversation = document.getElementById('conversation');
 const inputForm = document.getElementById('input-form');
 const inputField = document.getElementById('input-field');
+const startButton = document.getElementById('start-button');
 
 // Message d'introduction du chatbot
 const introductionMessage = "Bonjour! Je suis Aidan, votre conseiller commercial virtuel. Je vais vous poser quelques questions pour mieux comprendre vos besoins.";
@@ -12,15 +13,13 @@ window.addEventListener('load', () => {
     afficherMessageChatbot(introductionMessage);
 });
 
-// Ajouter un gestionnaire d'événements au formulaire
-inputForm.addEventListener('submit', function(event) {
-    event.preventDefault();
+// Ajouter un gestionnaire d'événements au clic sur le bouton "OK" pour commencer
+startButton.addEventListener('click', () => {
+    // Supprimer le bouton "OK" de la vue
+    startButton.style.display = 'none';
 
-    const userInput = inputField.value;
-    inputField.value = '';
-
-    // Gérer la réponse de l'utilisateur ici
-    traiterReponseUtilisateur(userInput);
+    // Commencer la série de questions
+    commencerQuestions();
 });
 
 // Fonction pour afficher un message du chatbot dans la conversation
@@ -32,8 +31,10 @@ function afficherMessageChatbot(message) {
     messageElement.scrollIntoView({ behavior: "smooth" });
 }
 
-// Fonction pour gérer la réponse de l'utilisateur
-function traiterReponseUtilisateur(reponse) {
-    // TODO: Implémentez la logique de traitement des réponses de l'utilisateur ici
-    // Vous pouvez poser des questions supplémentaires et présenter des services en fonction des réponses.
+// Fonction pour commencer la série de questions
+function commencerQuestions() {
+    // Vous pouvez poser la première question ici
+    poserQuestion("Quel est votre prénom ?");
 }
+
+// Reste du code pour traiter les réponses de l'utilisateur et présenter les services
